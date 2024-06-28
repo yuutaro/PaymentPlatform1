b0a6f575fb1a1ecfcb283090fa1a8ecf0cfb6cae
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2024-06-28 14:30:59 +0900

    rails/ ルーティング変更

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
