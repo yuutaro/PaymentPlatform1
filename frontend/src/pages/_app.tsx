206274873c8a0098b8573e93aaff33f4cfb88fc6
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-07-10 00:19:48 +0900

    UseGlobalState 実装

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+//import * as React from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
