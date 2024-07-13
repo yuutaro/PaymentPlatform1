f0cbdc644e11731e125710049ee7fb9f031731ec
yutaro-desktop <yuutaro.mikasa@gmail.com>
2024-07-13 15:22:24 +0900

    /backend全部削除

--- backend/app/controllers/api/v1/health_check_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::HealthCheckController < ApplicationController
-  def index
-    render json: { message: "Server is running!" }, status: :ok
-  end
-end
--- backend/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get "health_check", to: "health_check#index"
-    end
-  end
-end
