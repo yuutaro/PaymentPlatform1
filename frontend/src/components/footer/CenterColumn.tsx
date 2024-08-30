b89d4b2e65a325784c16286aa2732008f716bb69
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-08-30 14:45:30 +0900

    footerコンポーネント細分化

---
+++ frontend/src/components/footer/CenterColumn.tsx
@@ -0,0 +1,18 @@
+import Link from "../../../node_modules/next/link"
+const CenterColumn = () => {
+  return (
+    <>
+      <nav>
+        <h6 className="footer-title">Help & Guide</h6>
+        <a className="link link-hover">Art Squareとは</a>
+        <a className="link link-hover">オークション販売について</a>
+        <a className="link link-hover">抽選販売について</a>
+        <a className="link link-hover">一般販売について</a>
+        <a className="link link-hover">ポートフォリオについて</a>
+        <a className="link link-hover">一般販売について</a>
+      </nav>
+    </>
+  )
+}
+
+export default CenterColumn
