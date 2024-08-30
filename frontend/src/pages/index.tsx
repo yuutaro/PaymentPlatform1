c522631120c11ee46e45556399e39ec8fc7a0637
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-08-30 14:44:28 +0900

    header コンポーネント細分化

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,29 @@
+import camelcaseKeys from 'camelcase-keys'
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import useSWR from 'swr'
 import { fetcher } from '@/utils'
 
-const Index: NextPage = () => {
-  const url = 'http://localhost:3001/api/v1/health_check'
-  const { data, error } = useSWR(url, fetcher)
+type ItemProps = {
+  name: string
+  image: File
+  min_price: number
+}
 
-  /* FrontEnd作成のため一時的にコメントアウト
+const Index: NextPage = () => {
+  const url = 'http://localhost:3001/api/v1/items'
+
+  const { data, error } = useSWR(url, fetcher)
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
-  */
+
+  console.log(data)
+
+  const items = camelcaseKeys(data.items)
+
+  console.log(data.items)
+  
+
   return (
     <>
       <div className="w-full flex justify-center mb-48">
@@ -38,91 +51,28 @@ const Index: NextPage = () => {
           {/* いいね数や入札数、閲覧数が多い作品を表示 */}
           <div className="">
             <p className="text-3xl font-serif py-8">人気の絵画作品</p>
-            <div className="flex flex-wrap justify-center">
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
+            <div className="flex flex-wrap justify-center">      
+              {data.map((item: ItemProps, i: number) => (
+                <div className="flex flex-col">
+                  <div key={i} className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="h-[300px] w-[300px] object-contain"
+                    />
+                    <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
+                      <p className="text-white font-thin flex justify-end mr-2 ">
+                        ¥{item.min_price}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="mt-4 ml-4">{item.name}</p>
+                 
                 </div>
-              </div>
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
-                </div>
-              </div>
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
-                </div>
-              </div>
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
-                </div>
-              </div>
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
-                </div>
-              </div>
-              <div className="h-[300px] w-[300px] aspect-square flex relative justify-center items-center bg-zinc-100 shadow">
-                <Image
-                  src="/sample.jpg"
-                  width={200}
-                  height={200}
-                  alt="sample_img"
-                  className="p-1"
-                />
-                <div className="right-0 bottom-0 w-24 h-6 bg-black absolute m-1 bg-opacity-75">
-                  <p className="text-white font-thin flex justify-end mr-2 ">
-                    ¥10,000
-                  </p>
-                </div>
-              </div>
+              ))}
+
+              
+            
             </div>
           </div>
         </div>
