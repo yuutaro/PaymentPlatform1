a869a1ac0626e910c907ae94db36c60ee6c3e9f1
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-09-03 20:05:41 +0900

    Userモデル avatarカラム追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_134033) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_03_110123) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", comment: "作品名"
     t.string "discription", comment: "作品紹介"
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_29_134033) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "avatar", size: :long
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
