888779b3545cae13d0e45e2ac54ae5ac7f4542ff
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-09-23 20:39:51 +0900

    images型変更

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_03_123544) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_10_081923) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", comment: "作品名"
     t.string "discription", comment: "作品紹介"
     t.integer "min_price", comment: "価格"
     t.integer "amount", comment: "個数"
-    t.text "image", size: :long, comment: "作品画像URL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "state", comment: "公開状況"
+    t.json "images", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
