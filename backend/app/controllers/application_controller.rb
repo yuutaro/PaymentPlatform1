5ee16a4fd5cd5f495f2d979a4fae623ec7951f68
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-09-29 03:34:39 +0900

    新規アカウント登録時にname保存

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #新規アカウント登録時にnameを保存
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
